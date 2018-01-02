385ced4e7e56e0849f18a9623ff8e4bbf782e003
aopstudio <cenzhedong2@126.com>
2018-01-02 19:22:33 +0800

    正是加入了点菜功能，能够以可视化的方式展示出所点的菜以及总价格

--- iframe/cai.js
+++ iframe/cai.js
@@ -1,18 +1,20 @@
 
-new Vue({
+/*new Vue({
     el:'.meals',
     components:{
         'cai':{
-            template:'<div class="meal">\n' +
-            '        <img class="mealimage" v-bind:src=\'"../image/"+name+".jpg"\' v-bind:alt=name>\n' +
-            '        <div class="name">{{this.name}}</div>\n' +
-            '        <div class="price">{{this.price}}元</div>\n' +
-            '    </div>',
-            props:['name','price']
+            template:`<div class="meal">
+                    <img class="mealimage" v-bind:src=\'"image/"+name+".jpg"\' v-bind:alt=name>
+                    <div class="name">{{this.name}}</div>
+                    <div class="price">{{this.price}}元</div>
+                    <button v-on:click="add">选择</button>
+                    </div>`,
+            props:['name','price'],
+            methods:{
+                add:function () {
+                    console.log(this.name);
+                }
+            }
         }
-    }
-});
-
-function add() {
-    parent.window.document.getElementById()
-}
+    },
+});*/
--- js/rest.js
+++ js/rest.js
@@ -1,15 +1,37 @@
 var app=new Vue({
-    el: '#app,#ordered',
+    el: '#app,#ordered,.meals',
     data: {
         show: true,
         url:null,
         foodList:[
 
-        ]
+        ],
+        totalPrice:0
+    },
+    components:{
+        'cai':{
+            template:`<div class="meal">
+                    <img class="mealimage" v-bind:src=\'"image/"+name+".jpg"\' v-bind:alt=name>
+                    <div class="name">{{this.name}}</div>
+                    <div class="price">{{this.price}}元</div>
+                    <button v-on:click="add">选择</button>
+                    </div>`,
+            props:['name','price'],
+            methods:{
+                add:function () {
+                    app.foodList.push(this.name);
+                    app.totalPrice+=parseInt(this.price);
+                }
+            }
+        }
     },
     methods:{
-        add:function () {
-            this.foodList.push('锅包肉');
+        clearList:function () {
+            app.foodList.splice(0,app.foodList.length);
+            app.totalPrice=0;
+        },
+        information:function () {
+            var choose=window.confirm('您总共点了'+app.foodList.length+'个菜，消费'+app.totalPrice+'元');
         }
     }
 })
